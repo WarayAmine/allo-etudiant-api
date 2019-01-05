69c585912fdc18ae13e500588bc6234ea948d07e
waray <mohamedamine.waray@etudiant.univ-lille1.fr>
2019-01-05 22:23:10 +0100

    init project

---
+++ src/main/java/m2/eservices/alloetudiant/AlloetudiantApplication.java
@@ -0,0 +1,14 @@
+package m2.eservices.alloetudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloetudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloetudiantApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/m2/eservices/alloetudiant/AlloetudiantApplicationTests.java
@@ -0,0 +1,17 @@
+package m2.eservices.alloetudiant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlloetudiantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
