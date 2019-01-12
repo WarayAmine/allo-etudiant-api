b06a287f243e6ae3506002f712aa91c8ae28dfca
Waray <waray.amine@gmail.com>
2019-01-12 15:54:11 +0100

    added mongodb application properties

--- src/main/java/m2/eservices/alloetudiant/AlloetudiantApplication.java
+++ src/main/java/m2/eservices/alloetudiant/AlloetudiantApplication.java
@@ -2,6 +2,8 @@ package m2.eservices.alloetudiant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class AlloetudiantApplication {
@@ -9,5 +11,4 @@ public class AlloetudiantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlloetudiantApplication.class, args);
 	}
-
 }
