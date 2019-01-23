66891572b4d8ec105490e6ca6282a344aa2798db
Waray <waray.amine@gmail.com>
2019-01-23 22:47:48 +0100

    auth-security

---
+++ src/main/java/m2/eservices/alloetudiant/config/JWTAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package m2.eservices.alloetudiant.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import m2.eservices.alloetudiant.pojos.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super();
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response){
+        Profile profile;
+        try {
+            profile = new ObjectMapper().readValue(request.getInputStream(), Profile.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("*********** Username : "+profile.getEmail());
+        return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(profile.getEmail(),profile.getPassword())
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain filterChain, Authentication result) throws IOException, ServletException {
+        User springUser = (User) result.getPrincipal();
+        String jwtToken= Jwts.builder()
+                .setSubject(springUser.getUsername())
+                .setExpiration(new
+                        Date(System.currentTimeMillis()+ SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SecurityConstants.SECRET)
+                .claim("roles", springUser.getAuthorities())
+                .compact();
+
+        response.addHeader(SecurityConstants.HEADER_STRING, jwtToken);
+    }
+}
