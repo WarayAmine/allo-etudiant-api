834ea1ea5726c8bd36b004547cf4d0c6961ac8c0
Waray <waray.amine@gmail.com>
2019-03-08 12:03:04 +0100

    (fix) subjects in french

--- src/main/java/m2/eservices/alloetudiant/enumerations/Subject.java
+++ src/main/java/m2/eservices/alloetudiant/enumerations/Subject.java
@@ -1,9 +1,10 @@
 package m2.eservices.alloetudiant.enumerations;
 
 public enum Subject {
-    MATHS,
-    PHYSICS,
-    CHEMISTRY,
-    MECHANICS,
-    COMPUTER_SCIENCE
+    MATHEMATIQUES,
+    PHYSIQUE,
+    CHIMIE,
+    MECANIQUE,
+    INFORMATIQUE,
+    ELECTRONIQUE
 }
